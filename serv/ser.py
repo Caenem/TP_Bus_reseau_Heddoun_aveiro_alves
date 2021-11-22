436e532cd86f345ec2ac44642280525c19d0498a
Caenem <91149603+Caenem@users.noreply.github.com>
2021-11-22 09:41:41 +0100

    Update ser.py

--- serv/ser.py
+++ serv/ser.py
@@ -2,8 +2,8 @@ import serial
 
 ser = serial.Serial('/dev/serial0', 115200)
 print("Attente donnes")
-ser.write(b'!GET_T')
-x = ser.readline()
+ser.write(b'!GET_T') #ordre pour demander la température au STM32
+x = ser.readline() #on lit le port série jusqu'à un retour à la ligne
 print(x)
 ser.close()
 
